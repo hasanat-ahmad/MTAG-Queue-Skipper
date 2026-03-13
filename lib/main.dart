c5a2c292f18f5a9955290ef29e66b84a7f3efe60
hasanat-ahmad <sahibzadahasanatahmad@gmail.com>
2026-03-13 22:58:26 +0500

    feat: regsiter screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mtag_queue_skipper/screens/login_screen.dart';
+import 'package:mtag_queue_skipper/screens/register_screen.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: RegisterScreen(),
     );
   }
 }
