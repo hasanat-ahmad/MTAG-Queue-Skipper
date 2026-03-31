cb91355e3ed9adbabfbd2b3b1fdd85f11fcca7f1
hasanat-ahmad <sahibzadahasanatahmad@gmail.com>
2026-03-31 21:56:38 +0500

    fix: colors

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -61,7 +61,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(100),
-                      borderSide: const BorderSide(width: 1),
+                      borderSide: const BorderSide(width: 0),
                     ),
                     errorBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(100),
@@ -73,7 +73,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     focusedErrorBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(100),
                       borderSide: BorderSide(
-                        color: AppColors.primaryRed,
+                        color: AppColors.backgroundDark,
                         width: 1,
                       ),
                     ),
